dc1624d578460b7fc9c55381e69a7b97eea532bc
YwXuan <102135202+YwXuan@users.noreply.github.com>
2024-07-23 01:07:48 +0800

    acne-backend

---
+++ acne-backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const videoRoutes = require('./routes/videoRoutes');
+const quizRoutes = require('./routes/quizRoutes');
+const bkframe2Routes=require('./routes/bkframe2Routes.js');
+
+// 添加 cors 中间件
+app.use(cors());
+
+app.use('/api', videoRoutes);
+app.use('/api', quizRoutes);
+app.use('/api', bkframe2Routes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
